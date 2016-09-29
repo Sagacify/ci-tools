cbca04cbbee9231a7a45f36a3d3c13fd3e3d43d3
Louis Borsu <sat@satprod.net>
2016-09-29 10:58:30 +0200

    fix(java_check): moved in check function

--- run-sonar.sh
+++ run-sonar.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-if type -p java; then
-    echo found java executable in PATH
-    _java=java
-elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
-    echo found java executable in JAVA_HOME
-    _java="$JAVA_HOME/bin/java"
-else
-    echo "no java"
-    exit 1
-fi
-
-if [[ "$_java" ]]; then
-    version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
-    echo version "$version"
-    if [[ "$version" == *"1.8"* ]]; then
-        echo "version is 1.8"
-    else
-        echo "version is not 1.8"
-        exit 1
-    fi
-fi
-
 SONAR_VERSION="sonar-scanner-2.8"
 
 function install() {
@@ -65,6 +43,28 @@ function run() {
 }
 
 function check() {
+  if type -p java; then
+    echo found java executable in PATH
+    _java=java
+  elif [[ -n "$JAVA_HOME" ]] && [[ -x "$JAVA_HOME/bin/java" ]];  then
+      echo found java executable in JAVA_HOME
+      _java="$JAVA_HOME/bin/java"
+  else
+      echo "no java"
+      exit 1
+  fi
+
+  if [[ "$_java" ]]; then
+      version=$("$_java" -version 2>&1 | awk -F '"' '/version/ {print $2}')
+      echo version "$version"
+      if [[ "$version" == *"1.8"* ]]; then
+          echo "version is 1.8"
+      else
+          echo "version is not 1.8"
+          exit 1
+      fi
+  fi
+
   if [ -z $CI_PULL_REQUEST ] && [ "$CIRCLE_BRANCH" != "master" ] && [ "$CIRCLE_BRANCH" != "staging" ];
   then
     if [ -z $CI_API_TOKEN ];
