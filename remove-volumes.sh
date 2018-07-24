e44f04a14bbf26afa548be9d428c628c52e0ee4b
Quentin Gusbin <qg3393@hotmail.com>
2018-07-24 08:34:46 +0200

    feat(cci2): remove-volumes script

---
+++ remove-volumes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import yaml
+
+def remove_key_rec(mapping, removeKey):
+    mapping.pop(removeKey, None)
+    for key, value in mapping.iteritems():
+        if type(value) is dict:
+          remove_key_rec(value, removeKey)
+
+with open("docker-compose.yml", 'r') as stream:
+    try:
+        a = yaml.load(stream)
+        remove_key_rec(a, 'volumes')
+        print(a)
+    except yaml.YAMLError as exc:
+        print(exc)
+
+
+
