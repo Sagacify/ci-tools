4a83cab845df532c2597c4b20735176aefc9538a
Quentin Gusbin <quentin.gusbin@gmail.com>
2021-01-07 11:53:42 +0100

    feat(preview): remove

--- run-sonar-v2.sh
+++ run-sonar-v2.sh
@@ -100,8 +100,7 @@ function run() {
 		-Dsonar.projectKey=$SONAR_PROJECT_KEY \
 		-Dsonar.github.repository=$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME \
 		-Dsonar.github.pullRequest=${CI_PULL_REQUEST##*/} \
-		-Dsonar.github.oauth=$SAGA_STALIN_TOKEN \
-		-Dsonar.analysis.mode=preview;
+		-Dsonar.github.oauth=$SAGA_STALIN_TOKEN;
 	  elif [ "$CIRCLE_BRANCH" == "master" ];
 	    then
 		./$SONAR_EXTRACTED_FOLDER/bin/sonar-scanner $DEFAULT_SONAR_PARAMS \
